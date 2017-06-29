1895a940abf36133b064b63f3e232e4cd05b0a6f
vagrant <vagrant@homestead>
2017-06-29 07:07:06 +0000

    Finish user signup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,4 @@ Route::get('/help', 'StaticPagesController@help')->name('help');//帮助页
 Route::get('/about', 'StaticPagesController@about')->name('about');//关于页
 
 Route::get('/signup','UsersController@create')->name('signup');//注册
+Route::resource('users', 'UsersController');
